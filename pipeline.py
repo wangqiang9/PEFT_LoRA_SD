e36d98b0bced6de6e610270c0daf95a5bb285f60
XDUWQ <1300964705@qq.com>
2023-09-15 21:43:15 +0800

    update pipeline

--- pipeline.py
+++ pipeline.py
@@ -1,15 +1,18 @@
-import torch
-from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler
+from modelscope.models import Model
+from peft import PeftModel
+from modelscope.pipelines import pipeline
+from modelscope.utils.constant import DownloadMode, Tasks
+from diffusers import DiffusionPipeline
+from modelscope import snapshot_download
 
-model_id = "stabilityai/stable-diffusion-2-1"
+model_path = snapshot_download("AI-ModelScope/stable-diffusion-v2-1")
+model = Model.from_pretrained(model_path, device_map='auto')
 
-# Use the DPMSolverMultistepScheduler (DPM-Solver++) scheduler here instead
-pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
-pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
-pipe = pipe.to("cuda")
-
-prompt = "a photo of an astronaut riding a horse on mars"
-image = pipe(prompt).images[0]
-    
-image.save("astronaut_rides_horse.png")
-
+pipeline = DiffusionPipeline.from_pretrained(
+            '/mnt/workspace/.cache/modelscope/AI-ModelScope/stable-diffusion-v2-1')
+pipeline = pipeline.to('cuda')
+pipeline.unet = PeftModel.from_pretrained(pipeline.unet, './results/unet')
+prompt = "a dog"
+for index in range(20):
+    image = pipeline(prompt).images[0]
+    image.save(f"./lora_result_{index}.png")
